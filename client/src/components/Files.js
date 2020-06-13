2a0e817c70370ff89e620a39b937133756380639
Ash <ashutoshmishra413@gmail.com>
2020-06-13 12:15:58 -0400

    Same as before

--- client/src/components/Files.js
+++ client/src/components/Files.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import File from "./File";
 
 export class Files extends Component {
   render() {
-    return <div>Files Go Here</div>;
+    return (
+      <div>
+        <h2>Uploaded Files</h2>
+        {this.props.files.map((file) => (
+          <File file={file} key={file.id} />
+        ))}
+      </div>
+    );
   }
 }
 

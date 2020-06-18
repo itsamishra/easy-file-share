41bc84d93a915febac698a7a339540c66ad42c98
Ash <ashutoshmishra413@gmail.com>
2020-06-17 23:05:14 -0400

    Updated header with updated name

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ export class Header extends Component {
   render() {
     return (
       <div>
-        <h1 style={headerStyle}>File Transfer Room</h1>
+        <h1 style={headerStyle}>Easy File Share</h1>
       </div>
     );
   }
